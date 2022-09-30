7a7e081be17d8a9192a14135b66e8ce644cbf9bd
xucelin <xucl@300624.com>
2022-09-30 16:06:15 +0800

    codeblocks_deps

--- tools/codeblocks_proj_deps/version.h
+++ tools/codeblocks_proj_deps/version.h
@@ -1,12 +1,12 @@
 #pragma once
 
 /* follow macro is set by cmake::project(**) and cmake::config_file  */
-#define project_name                      "codeblocks_to_cmake"
+#define project_name                      "codeblocks_proj_dependencies"
 #define project_VERSION_MAJOR 1
 #define project_VERSION_MINOR 1
 #define project_VERSION_PATCH 
 #define project_VERSION_TWEAK 
 #define project_VERSION       1.1
-#define CMAKE_CURRENT_LIST_DIR  "/home/ws/src/code/jungle/tools/codeblocks_to_cmake"
-#define project_DESCRIPTION "trans .cbp file to CMakeLists"
+#define CMAKE_CURRENT_LIST_DIR  "/home/ws/src/code/jungle/tools/codeblocks_proj_deps"
+#define project_DESCRIPTION "analyze dependencies"
   
